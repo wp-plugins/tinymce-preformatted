eff02fdb11e2741c88c8d8e9dc786f51bf94ed31
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-22 16:03:33 +0000

    update to 0.2
    git-svn-id: https://plugins.svn.wordpress.org/tinymce-preformatted/trunk@314540 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinymce_preformatted.php
+++ tinymce_preformatted.php
@@ -4,7 +4,7 @@ Plugin Name: TinyMCE Preformatted
 Plugin URI: http://firegoby.theta.ne.jp/wp/mce_preformatted
 Description: Insert preformatted source.
 Author: Takayuki Miyauchi
-Version: 0.1
+Version: 0.2
 Author URI: http://firegoby.theta.ne.jp/
 */
 
