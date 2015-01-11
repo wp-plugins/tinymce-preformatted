4e0ae956573a17288c94e51af4d0ebb97b40fbb9
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-11 22:36:28 +0000

    ver 0.6.0
    git-svn-id: https://plugins.svn.wordpress.org/tinymce-preformatted/tags/0.6.0@1065445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mce_plugins/4.0/plugins/preformatted/plugin.js
+++ mce_plugins/4.0/plugins/preformatted/plugin.js
@@ -1,48 +1,56 @@
-
-
 tinymce.PluginManager.add('preformatted', function(editor) {
     function showDialog() {
         var e = tinymce.activeEditor.selection.getNode();
+        console.log(editor.selection.getContent());
+        console.log(editor.selection.getNode().innerHTML);
         if ( e.nodeName == 'CODE' ) {
             editor.execCommand('mceRemoveNode', false, editor.dom.getParent(e, 'CODE'));
             editor.nodeChanged();
         } else if (editor.selection.isCollapsed() || e.nodeName == 'PRE') {
-            tinymce.activeEditor.selection.select(editor.dom.getParent(e, 'PRE'));
-            editor.windowManager.open({
-                title: "Source code",
-                body: {
-                    type: 'textbox',
-                    name: 'preformatted',
-                    multiline: true,
-                    minWidth: editor.getParam("code_dialog_width", 600),
-                    minHeight: editor.getParam("code_dialog_height", Math.min(tinymce.DOM.getViewPort().h - 200, 500)),
-                    value: tinymce.activeEditor.selection.getContent({format: 'text'}),
-                    spellcheck: false,
-                    style: 'direction: ltr; text-align: left'
-                },
-                onSubmit: function(e) {
-                    // We get a lovely "Wrong document" error in IE 11 if we
-                    // don't move the focus to the editor before creating an undo
-                    // transation since it tries to make a bookmark for the current selection
-                    editor.focus();
-
-                    editor.undoManager.transact(function() {
-                        editor.insertContent('<pre>' + escapeHtml(e.data.preformatted) + '</pre>');
-                    });
-
-                    editor.selection.setCursorLocation();
-                    editor.nodeChanged();
-                }
-            });
+            var node = tinymce.activeEditor.selection.select(editor.dom.getParent(e, 'PRE'));
+            setPre();
         } else {
-            editor.undoManager.transact(function() {
-                tinymce.activeEditor.selection.setNode(tinymce.activeEditor.dom.create(
-                    'code',
-                    {},
-                    editor.selection.getContent()
-                ));
-            });
+            setCode();
         }
+    }
+
+    function setPre() {
+        editor.windowManager.open({
+            title: "Source code",
+            body: {
+                type: 'textbox',
+                name: 'preformatted',
+                multiline: true,
+                minWidth: editor.getParam("code_dialog_width", 600),
+                minHeight: editor.getParam("code_dialog_height", Math.min(tinymce.DOM.getViewPort().h - 200, 500)),
+                value: tinymce.activeEditor.selection.getContent({format: 'text'}),
+                spellcheck: false,
+                style: 'direction: ltr; text-align: left'
+            },
+            onSubmit: function(e) {
+                // We get a lovely "Wrong document" error in IE 11 if we
+                // don't move the focus to the editor before creating an undo
+                // transation since it tries to make a bookmark for the current selection
+                editor.focus();
+
+                editor.undoManager.transact(function() {
+                    editor.insertContent('<pre>' + escapeHtml(e.data.preformatted) + '</pre><p></p>');
+                });
+
+                editor.selection.setCursorLocation();
+                editor.nodeChanged();
+            }
+        });
+    }
+
+    function setCode() {
+        editor.undoManager.transact(function() {
+            tinymce.activeEditor.selection.setNode(tinymce.activeEditor.dom.create(
+                'code',
+                {},
+                editor.selection.getContent()
+            ));
+        });
     }
 
     function escapeHtml(str) {
@@ -72,4 +80,3 @@ tinymce.PluginManager.add('preformatted', function(editor) {
         onclick: showDialog
     });
 });
-
